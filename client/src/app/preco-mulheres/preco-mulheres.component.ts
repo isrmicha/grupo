8e910931736f0c71cdc444c51de0821066f42b64
Israel <isrmicha@gmail.com>
2019-04-12 00:19:55 -0300

    aff

--- client/src/app/preco-mulheres/preco-mulheres.component.ts
+++ client/src/app/preco-mulheres/preco-mulheres.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType } from 'chart.js';
+import { Color } from 'ng2-charts';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-preco-mulheres',
@@ -6,10 +9,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preco-mulheres.component.scss']
 })
 export class PrecoMulheresComponent implements OnInit {
+  constructor(public http: HttpClient) { }
+  public bubbleChartOptions: ChartOptions = {
+    responsive: true,
+    scales: {
+      xAxes: [
+        {
+          ticks: {
+            min: 0,
+            max: 10,
+          }
+        }
+      ],
+      yAxes: [
+        {
+          ticks: {
+            min: 0,
+            max: 10,
+          }
+        }
+      ]
+    }
+  };
+  public bubbleChartType: ChartType = 'bubble';
+  public bubbleChartLegend = true;
 
-  constructor() { }
+  public bubbleChartData = [];
 
-  ngOnInit() {
+  public bubbleChartColors: Color[] = [
+    {
+      backgroundColor: [
+        'red',
+        'green',
+        'blue',
+        'purple',
+        'yellow',
+        'brown',
+        'magenta',
+        'cyan',
+        'orange',
+        'pink'
+      ]
+    }
+  ];
+  async ngOnInit() {
+    let rows: any = await this.http.get('https://grupogado.herokuapp.com/precoMulheres').toPromise()
+    rows.map((row, index) => {
+      console.log(row)
+      this.bubbleChartData[index] =
+        {
+          data: [
+            { x: row[1], y: row[2], r: 5 }
+          ],
+          label: row[0],
+          backgroundColor: 'green',
+          borderColor: 'blue',
+          hoverBackgroundColor: 'purple',
+          hoverBorderColor: 'red',
+        }
+    })
+    console.log(this.bubbleChartData)
   }
-
 }
