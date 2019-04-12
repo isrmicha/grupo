2062f175e0c34d50ef87243f84017d475df0e62f
Israel <isrmicha@gmail.com>
2019-04-11 23:14:12 -0300

    maldito cors

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
+
 const port = process.env.PORT || 3000
 const listaEsforcoSalario = require('./googleSheets').listaEsforcoSalario
 const listaPrecoMulheres = require('./googleSheets').listaPrecoMulheres
 
 
 
-
-
+app.use(cors())
 app.get('/', (req, res) => express.static('public'))
 app.get('/esforcoSalario', async(req, res) => {
   return res.send(await listaEsforcoSalario())
