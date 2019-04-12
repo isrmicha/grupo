496008a17aa06a37b920ee13c21f8eb5c05489f1
Israel <isrmicha@gmail.com>
2019-04-11 23:09:07 -0300

    q

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const listaEsforcoSalario = require('./googleSheets').listaEsforcoSalario
+const listaPrecoMulheres = require('./googleSheets').listaPrecoMulheres
 
 
 
@@ -9,10 +10,10 @@ const listaEsforcoSalario = require('./googleSheets').listaEsforcoSalario
 
 app.get('/', (req, res) => express.static('public'))
 app.get('/esforcoSalario', async(req, res) => {
-  res.send(await listaEsforcoSalario())
+  return res.send(await listaEsforcoSalario())
 })
 app.get('/precoMulheres', async(req, res) => {
-  res.send(await listaEsforcoSalario())
+  return res.send(await listaPrecoMulheres())
 })
 
 app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`))
